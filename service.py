d301762bc0bb250060aa0740c5120542d4b8e720
BraganholoGui <braganhologuilherme@gmail.com>
2024-03-08 01:17:42 -0300

    connect to new api

--- service.py
+++ service.py
@@ -11,17 +11,19 @@ def goodbye():
     return jsonify({'message': 'Goodbye from the blueprint!'})
 
 @service.route('/aaa')
-def index():
-    # Faz uma requisição GET para uma API externa (por exemplo, a API de dados do COVID-19)
-    response = requests.get('https://api.covid19api.com/summary')
+def covid():
+    try:
+        headers = {
+            'AccessKey': {'YourKeyValueHere'}  # Defina o cabeçalho User-Agent de acordo com sua aplicação
+        }
+        response = requests.get('https://services.apistore.dev/apistore/countries/v1', headers=headers)
 
-    # Verifica se a requisição foi bem sucedida (código de status 200)
-    if response.status_code == 200:
-        # Converte a resposta para um objeto JSON
-        data = response.json()
-        # Passa os dados para o template e renderiza o template
-        print(data)
-        return render_template('index.html', data=data)
-    else:
-        # Retorna uma mensagem de erro caso a requisição não tenha sido bem sucedida
-        return jsonify({'error': 'Failed to fetch data'}), 500
+        if response.status_code == 200:
+            data = response.json()
+            print(data)
+            return render_template('index.html', data=data)
+        else:
+            print('data')
+            return jsonify({'error': 'Failed to fetch data'}), 500
+    except:
+        return jsonify({'error': 'Failed to connect to API'}), 500
