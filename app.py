fe5a44b142300632a55489fc69f4a624e7ea8526
BraganholoGui <braganhologuilherme@gmail.com>
2024-03-07 23:44:49 -0300

    adjustments

--- app.py
+++ app.py
@@ -2,13 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# def layout(template_name):
-#     def decorator(view_func):
-#         def wrapper(*args, **kwargs):
-#             return render_template('layout.html', content=view_func(*args, **kwargs), template_name=template_name)
-#         return wrapper
-#     return decorator
-
 @app.route('/')
 def index():
     return render_template('index.html')
