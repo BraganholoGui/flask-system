24f886e8b2bced1329cebfed1d25bdd37c8ee61f
BraganholoGui <braganhologuilherme@gmail.com>
2024-03-18 22:09:44 -0300

    repositories

--- service.py
+++ service.py
@@ -2,29 +2,19 @@ from flask import Flask, Blueprint, jsonify, render_template
 import requests
 service = Blueprint('service', __name__)
 
-@service.route('/hello')
-def hello():
-    return jsonify({'message': 'Hello from the blueprint!'})
-
-@service.route('/goodbye')
-def goodbye():
-    return jsonify({'message': 'Goodbye from the blueprint!'})
-
-@service.route('/aaa')
-def covid():
+@service.route('/repositories')
+def repositories():
     try:
-        headers = {
-            'AccessKey': {'YourKeyValueHere'}  # Defina o cabeçalho User-Agent de acordo com sua aplicação
-        }
-        response = requests.get('https://api.thecatapi.com/v1/images/search?limit=20')
 
+        response = requests.get('https://api.thecatapi.com/v1/images/search?limit=20')
         print(response)
         if response.status_code == 200:
             data = response.json()
             print(data)
-            return render_template('publishments.html', datas=data)
+            return render_template('repositories.html', datas=data)
         else:
             print('data')
             return jsonify({'error': 'Failed to fetch data'}), 500
-    except:
+    except requests.exceptions.RequestException as e:
+        print(e)
         return jsonify({'error': 'Failed to connect to API'}), 500
